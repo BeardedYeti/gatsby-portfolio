644d7218ed4ac76cedadf816fb81fc695fd99c35
Colin Smith <cmsgoboston41@gmail.com>
2018-02-05 17:08:52 -0500

    Add dummy react components and links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from "react"
+import Link from "gatsby-link";
 
-export default () => <div>Hello world!</div>
+export default () => 
+
+	<div style={{ color: `blue` }}>
+		<h1>Hello Yeti!</h1>
+		<p>Woah!</p>
+		<img src="https://source.unsplash.com/random/400x200" alt="" />
+		<br />
+		<div>
+			<Link to="/page-2/">Link</Link>
+		</div>
+		<div>
+			<Link to="/counter/">Counter</Link>
+		</div>
+	</div>
---
+++ src/pages/page-2.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Link from "gatsby-link";
+
+export default () => (
+	<div>
+		<p>Hellow from Page 2</p>
+		<Link to="/">Home</Link>
+	</div>
+);
